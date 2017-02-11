d4e8a5a52076902a93024309756a7b19c86f88d3
VadVor <vadimvoronov192@gmail.com>
2017-02-11 14:43:57 +0300

    tupls

--- Chapter_3/test.py
+++ Chapter_3/test.py
@@ -9,6 +9,7 @@ Seating = collections.namedtuple("Seating", "minimum maximum")
 aircraft = Aircraft("Airbus", "A320200", Seating(100, 220))
 p = aircraft.seating.maximum
 
+
 total = 0
 for sale in sales:
     total += sale.quantity * sale.price
