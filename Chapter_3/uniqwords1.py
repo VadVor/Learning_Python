c0b9c15c200737be7b71a4d50a1c575e4d39baba
VadVor <vadimvoronov192@gmail.com>
2017-03-18 10:58:32 +0300

    Типы коллекций

---
+++ Chapter_3/uniqwords1.py
@@ -0,0 +1,16 @@
+import string
+import sys
+
+words = {}
+strip = string.whitespace + string.punctuation + string.digits + "\"'"
+for filename in sys.argv[1:]:
+    for line in open(filename, encoding="UTF-8"):
+        for word in line.lower().split():
+            word = word.strip(strip)
+            if len(word) > 2:
+                words[word] = words.get(word, 0) + 1
+for word in sorted(words):
+    print("'{0}' occurs {1} times".format(word, words[word]))
+
+
+
