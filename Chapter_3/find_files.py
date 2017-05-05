eda52d4b2251007e7bbf1af2ff152e263dc58a79
VadVor <vadimvoronov195@gmail.com>
2017-05-05 14:32:27 +0300

    Create find_files.py

---
+++ Chapter_3/find_files.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import fnmatch
+import win32com.client
+
+obj = win32com.client.GetObject("LDAP://DC=asb/DC=bb/DC=f700/OU=fil700")
+obj.Filter = ["Computer"]
+for comp in object:
+    print (comp.Name)
+
+'''for comp in object:
+    with open('d:\ccccccc.txt', "a") as file:
+        file.write(comp.Name+"\n")
+file.close()'''
+
+''' exclude = ['$Recycle.Bin','d:\$RECYCLE.BIN','Windows'] 
+for root in ['c:\\', 'd:\\']:
+    for folder, subdirs, files in os.walk(root, topdown=True):
+        subdirs[:] = [d for d in subdirs if d not in exclude]
+        for pattern in ['*.arj', '*.exe', '*.iso', '*.rar', '*.zip', '*.msi', '*.7z', '*.com', '*.mp1', '*.mp2', '*.mp3', '*.mp4', '*.mpg', '*.avi' , '*.mpeg', '*.mkv', '*.flv', '*.wma', '*.mov', '*.asf', '*.7zip']:
+            for filename in fnmatch.filter(files, pattern): 
+                fullname = os.path.join(folder, filename)
+                with open('d:\qqqqqqqq.txt', "a") as file:
+                    file.write(fullname+"\n")
+file.close() '''
+        
+    
