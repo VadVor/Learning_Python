c1ee6304bd03eddfe5d1298c9e90eba1dce7a581
VadVor <vadimvoronov192@gmail.com>
2017-05-25 20:39:48 +0300

    Типы коллекций

--- Chapter_3/statistics.py
+++ Chapter_3/statistics.py
@@ -1,5 +1,6 @@
 import collections
 import sys
+import math
 
 
 Statistics = collections.namedtuple("Statistics", "mean mode median std_dev")
@@ -33,11 +34,58 @@ def read_data(filename, numbers, frequencies):
 
 
 def calculate_statistics(numbers, frequencies):
-    mean = sum(numbers) / len(numbers))
+    mean = sum(numbers) / len(numbers)
     mode = calculate_mode(frequencies, 3)
     median = calculate_median(numbers)
     std_dev = calculate_std_dev(numbers, mean)
     return Statistics(mean, mode, median, std_dev)
 
 
+def calculate_mode(frequencies, maximum_modes):
+    highest_frequency = max(frequencies.values())
+    mode = [number for number, frequency in frequencies.items()
+            if frequency == highest_frequency]
+    if not (1 <= len(mode) <= maximum_modes):
+        mode = None
+    else:
+        mode.sort()
+    return mode
 
+
+def calculate_median(numbers):
+    numbers = sorted(numbers)
+    middle = len(numbers) // 2
+    median = numbers[middle]
+    if len(numbers) % 2 == 0:
+        median = (median + numbers[middle - 1]) / 2
+    return median
+
+
+def calculate_std_dev(numbers, mean):
+    total = 0
+    for number in numbers:
+        total += ((number - mean) ** 2)
+    variance = total / (len(numbers) - 1)  # variance is n-1
+    return math.sqrt(variance)  # std. dev.
+
+
+def print_results(count, statistics):
+    real = "9.2f"
+
+    if statistics.mode is None:
+        modeline = ""
+    elif len(statistics.mode) == 1:
+        modeline = "mode      = {0:{fmt}}\n".format(
+                statistics.mode[0], fmt=real)
+    else:
+        modeline = ("mode      = [" + ", ".join(["{0:.2f}".format(m)for m in statistics.mode]) + "]\n")
+    print("""\
+    count     = {0:6} 
+    mean      = {1.mean:{fmt}} 
+    median    = {1.median:{fmt}} 
+    {2}\
+    std. dev. = {1.std_dev:{fmt}}"""
+          .format(count, statistics, modeline, fmt=real))
+
+
+main()
