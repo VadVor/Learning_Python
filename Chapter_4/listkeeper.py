cbdf039a6a241e44bab326b3078c0c59ab3e7442
VadVor <vadimvoronov195@gmail.com>
2017-06-19 17:07:04 +0300

    Update listkeeper.py

--- Chapter_4/listkeeper.py
+++ Chapter_4/listkeeper.py
@@ -52,12 +52,13 @@ def get_integer(message, name="integer", default=None, minimum=0, maximum=100, a
             print("ERROR {0} must be an integer".format(name))
 
 
-def add_new_file_name():
-    file_name = get_string("Choose file name: ", "filename")
+def add_new_file_name(file_name,action):
     if not file_name:
-        raise CancelledError()
-    if not file_name.endswith(".lst"):
-        file_name += ".lst"
+        file_name = get_string("Choose file name: ", "filename")
+        if not file_name:
+            raise CancelledError()
+        if not file_name.endswith(".lst"):
+            file_name += ".lst"
     print("\n--No items are in the list--")
     action = get_string("[A]dd [Q]uit", "new_file", default="a")
     return file_name, action
@@ -67,92 +68,112 @@ def read_record_file(file_name, action_items):
     list_items = []
     saved = None
     while True:
-        if action_items in {"a", "A"}:
-            add_string = get_string("Add item ", "add_string")
-            list_items.append(add_string)
-            for l, item in enumerate(list_items, start=1):
-                print("{0}. {1}".format(l, item))
-            saved = False
-            action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
-            continue
-        elif action_items in {"d", "D"}:
-            del_string = get_integer("Delete item number (or 0 to cancel)", "del_string")
-            if del_string > 0:
-                list_items.pop(del_string-1)
-                for l, item in enumerate(list_items, start=1):
-                    if len(list_items) >= 1:
-                        print("{0}. {1}".format(l, item))
-                saved = False
-                action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
-                continue
-            elif del_string == 0:
-                for l, item in enumerate(list_items, start=1):
-                    if len(list_items) >= 1:
-                        print("{0}. {1}".format(l, item))
-                saved = False
-                action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
-                continue
-        elif action_items in {"s", "S"}:
-            fh = None
-            try:
-                fh = open(file_name, "w", encoding="utf-8")
-                for l, item in enumerate(list_items, start=1):
-                    fh.write("{1}\n".format(l, item))
-                print("Saved {0} items to {1}".format(l, file_name))
-                action_items = get_string("Press Enter to continue...", "add_string")
-                print("\n")
-                if action_items is not None:
-                    for l, item in enumerate(list_items, start=1):
-                        print("{0}. {1}".format(l, item))
-            except EnvironmentError as err:
-                print("ERROR", err)
-            finally:
-                if fh is not None:
-                    fh.close()
-                    saved = True
-                    action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
-            continue
-        elif action_items in {"o"}:
-            try:
-                for line in open(file_name, "r", encoding="utf-8"):
-                    list_items.append(line.rstrip())
-                for l, item in enumerate(list_items, start=1):
+        try:
+            if action_items in {"a", "A"}: # добавление элемента
+                add_string = get_string("Add item ", "add_string")
+                list_items.append(add_string)
+                for l, item in enumerate(sorted(list_items, key=str.lower), start=1):
                     print("{0}. {1}".format(l, item))
-            except EnvironmentError as err:
-                print("ERROR", err)
-            finally:
-                    saved = True
-                    action_items = get_string("[A]dd [D]elete [Q]uit ", "add_string", default="a")
-            continue
-        elif action_items in {"q", "Q"}:
-            if saved == True:
-                break
-            elif saved == False:
-                answer = get_string("Saved unseved changes (y/n)", "answer",default="y")
-                if answer in {"y", "Y"}:
-                    fh = file_name
+                saved = False
+                action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
+                continue
+            elif action_items in {"d", "D"}: # удаление элимента
+                del_string = get_integer("Delete item number (or 0 to cancel)", "del_string")
+                if del_string > 0:
+                    list_items.pop(del_string-1)
+                    for l, item in enumerate(list_items, start=1):
+                        if len(list_items) >= 1:
+                            print("{0}. {1}".format(l, item))
+                    saved = False
+                    action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
+                    continue
+                elif del_string == 0:
+                    for l, item in enumerate(list_items, start=1):
+                        if len(list_items) >= 1:
+                            print("{0}. {1}".format(l, item))
+                    saved = False
+                    action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
+                    continue
+            elif action_items in {"s", "S"}: #запись элементов
+                fh = None
+                try:
                     fh = open(file_name, "w", encoding="utf-8")
                     for l, item in enumerate(list_items, start=1):
-                        fh.write("{0}. {1}\n".format(l, item))
+                        fh.write("{1}\n".format(l, item))
                     print("Saved {0} items to {1}".format(l, file_name))
-                    fh.close()
+                    action_items = get_string("Press Enter to continue...", "add_string")
+                    print("\n")
+                    if action_items is not None:
+                        for l, item in enumerate(list_items, start=1):
+                            print("{0}. {1}".format(l, item))
+                except EnvironmentError as err:
+                    print("ERROR", err)
+                finally:
+                    if fh is not None:
+                        fh.close()
+                        saved = True
+                        action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
+                continue
+            elif action_items in {"o"}: # чтение записей файла
+                try:
+                    for line in open(file_name, "r", encoding="utf-8"):
+                        list_items.append(line.rstrip())
+                    for l, item in enumerate(sorted(list_items,key = str.lower), start=1):
+                        print("{0}. {1}".format(l, item))
+                except EnvironmentError as err:
+                    print("ERROR", err)
+                finally:
+                    saved = True
+                    action_items = get_string("[A]dd [D]elete [Q]uit ", "add_string", default="a")
+                continue
+            elif action_items in {"q", "Q"}: # выход из программы
+                if saved == True:
                     break
+                elif saved == False:
+                    answer = get_string("Saved unseved changes (y/n)", "answer",default="y")
+                    if answer in {"y", "Y"}:
+                        fh = file_name
+                        fh = open(file_name, "w", encoding="utf-8")
+                        for l, item in enumerate(list_items, start=1):
+                            fh.write("{1}\n".format(l, item))
+                        print("Saved {0} items to {1}".format(l, file_name))
+                        fh.close()
+                        break
+                    else:
+                        break
                 else:
                     break
             else:
-                break
-    return True
-number_file = 0
-list_files = [name for name in os.listdir(".") if name.endswith(".lst")]
-if len(list_files) == 0:
-    filename = add_new_file_name()
-    read_record_file(filename)
-else:
-    for n, file in enumerate(list_files, start=1):
-        print("{0}. {1}".format(n, file))
-    number_file = get_integer("Choose number or '"'0'"' for add new file ", "number_file")
-    if number_file == 0:
-        filename, action_asw = add_new_file_name()
-        read_record_file(filename, action_asw)
-    else:
-        read_record_file("".join(list_files[number_file-1]), action_items = "o")
+                print("ERROR: invalid choice--enter one of 'AaDdSsQq'")
+                action_items = get_string("Press Enter to continue...", "add_string")
+                if action_items is not None:
+                    action_items = get_string("[A]dd [D]elete [S]ave [Q]uit ", "add_string", default="a")
+        except ValueError as err:
+            print(err)
+            
+def main():
+    while True:
+        number_file = 0
+        list_files = [name for name in os.listdir(".") if name.endswith(".lst")]
+        if len(list_files) == 0:
+            filename = add_new_file_name()
+            read_record_file(filename)
+        else:
+            for n, file in enumerate(list_files, start=1):
+                print("{0}. {1}".format(n, file))
+            number_file = get_integer("Choose number or '"'0'"' for add new file ", "number_file")
+            if number_file == 0:
+                filename, action_asw = add_new_file_name()
+                read_record_file(filename, action_asw)
+            else:
+                if os.stat(list_files[number_file-1]).st_size == 0: 
+                    filename, action_asw = add_new_file_name(list_files[number_file-1],action="a")
+                    if action_asw in {"A", "a"}:
+                        read_record_file("".join(list_files[number_file-1]), action_items = "a")
+                        break
+                    else:
+                        break
+                else:
+                    read_record_file("".join(list_files[number_file-1]), action_items = "o")
+                    break
+main()
