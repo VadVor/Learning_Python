ce5a9eedeed4078cdaf7934047248fd085c2866f
VadVor <vadimvoronov195@gmail.com>
2017-07-07 15:42:24 +0300

    Update findfiles.py

--- Chapter_5/findfiles.py
+++ Chapter_5/findfiles.py
@@ -3,20 +3,20 @@ import datetime
 import collections
 
 patth="d://install//"
-data  = collections.defaultdict(list)
+# data  = collections.defaultdict(list)
+data = []
+list_for_print = []
 
 for root,dirs,files in os.walk(patth):
     dirs[:] = [dir for dir in dirs if not dir.startswith(".")]
     for filename in files:
         fullname = os.path.join(root, filename)
         mod_timestamp = datetime.datetime.fromtimestamp(os.path.getmtime(fullname))
-        key = (mod_timestamp, os.path.getsize(fullname))
-        data[key].append(fullname)
-
-for size, filename in sorted(data):
-    names = data[(size, filename)]
-    if len(names)>1:
-        print("{0} ({1} bytes) may be duplicated " "({2} files):".format(filename, size, len(names)))
-    for name in names:
-        print("\t{0}".format(name))
+        data.append((mod_timestamp, os.path.getsize(fullname), fullname))
+    break
+data.sort(key=lambda tup: tup[1])
+for datta in data:
+    list_for_print.append("{0:.19}    {1:>}     {2:<} ".format(str(datta[0]), str(datta[1]).strip(), str(datta[2]).strip()))
+for t in list_for_print:
+    print(t)   
 
