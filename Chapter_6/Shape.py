46e145baf328a7807e9d16145c2b16acc766f594
VadVor <vadimvoronov195@gmail.com>
2017-07-24 15:26:11 +0300

    Update Shape.py

--- Chapter_6/Shape.py
+++ Chapter_6/Shape.py
@@ -19,7 +19,26 @@ class Point:
     def __str__(self):
         return "({0.x!r}, {0.y!r})".format(self)
 
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+class Circle(Point):
+    def __init__(self, radius, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+    
+    def edge_distance_from_origin(self):
+        return abs(self.distance_from_origin()-self.radius)
+    
+    def area(self):
+        return math.pi * (self.radius ** 2)
+    
+    def circumference(self):
+        return 2 * math.pi * self.radius
+    
+    def __eq__(self, other):
+        return self.radius == other.radius and super().__eq__(other)
+    
+    def __repr__(self):
+        return "Circle({0.radius!r}, {0.x!r}, {0.y!r})".format(self)
+    
+    def __str__(self):
+        return repr(self)
+    
