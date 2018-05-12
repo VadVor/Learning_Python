8ee0bb8cb2ec74fd6c678224671645ab4751f609
Вадим <vadimvoronov195@gmail.com>
2018-05-12 18:34:15 +0300

    ООП (часть 1)

--- Chapter_6/test_obj.py
+++ Chapter_6/test_obj.py
@@ -12,7 +12,7 @@ midx, midy = width // 2, height // 2
 image = image.Image(width, height, img, "#F0F0F0")
 for x in range(width):
     for y in range(height):
-        if x < 6 or x >= width - 5 or y < 5 or y >= height - 5:
+        if x < 5 or x >= width - 5 or y < 5 or y >= height - 5:
             image[x, y] = border_color
         elif midx - 20 < x < midx + 20 and midy - 20 < y < midy + 20:
             image[x, y] = square_color
